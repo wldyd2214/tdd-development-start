6878de69eb628fa1daeb574bf2d907d02cb854c3
wldyd2214 <wldyd2214@naver.com>
2024-05-20 00:01:54 +0900

    [feat] 테스트 케이스 개발 완료로 인한 패키지 변경 및 리팩토링 작업

--- src/main/java/sample/tdd/unit/chap03/ExpiryDateCalculator.java
+++ src/main/java/sample/tdd/unit/chap03/ExpiryDateCalculator.java
@@ -6,17 +6,32 @@ import java.time.YearMonth;
 public class ExpiryDateCalculator {
     public LocalDate calculateExpiryDate(PayData payData) {
         //int addedMonths = payData.getPayAmount() == 100_000 ? 12 : payData.getPayAmount() / 10_000;
-        int addedMonths = addedMonthCalculator(payData.getPayAmount()); // 메소드로 기능으로 구현하여 리펙토링
+        int addedMonths = addedMonthCalculator2(payData.getPayAmount()); // 메소드로 기능으로 구현하여 리펙토링
 
         if(payData.getFirstBillingDate() != null) {
-            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+            return expiryDateUsingFirstBillingDate2(payData, addedMonths);
         }
 
         return payData.getBillingDate().plusMonths(addedMonths);
     }
 
     private int addedMonthCalculator(int payAmount) {
-        return payAmount == 100_000 ? 12 : payAmount / 10_000;
+        return payAmount >= 100_000 ? 12 : payAmount / 10_000;
+    }
+
+    private int addedMonthCalculator2(int payAmount) {
+        int addedMonths = payAmount / 10_000;
+
+        if (addedMonths < 10) return addedMonths;
+
+        int resultMonths = 0;
+        int year = addedMonths / 10;
+        int month = addedMonths % 10;
+
+        resultMonths = year * 12;
+        resultMonths += month;
+
+        return resultMonths;
     }
 
     private LocalDate expiryDateUsingFirstBillingDate(PayData payData, int addedMonths) {
@@ -36,7 +51,7 @@ public class ExpiryDateCalculator {
 
     private LocalDate expiryDateUsingFirstBillingDate2(PayData payData, int addedMonths) {
         LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-        if(isNotSameDayOfMonth(payData.getBillingDate(), candidateExp)) {
+        if(isNotSameDayOfMonth(payData.getFirstBillingDate(), candidateExp)) {
             final int dayLenOfCandiMon = lastDayOfMonth(candidateExp);
             // 처음 납부일에 대한 일수를 가져온다
             final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
